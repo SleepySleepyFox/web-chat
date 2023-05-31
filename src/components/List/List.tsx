0da002f3d1f5892200c82073f7c1839cbe51b29c
SleepySleepyFox <ShkrebNsh@gmail.com>
2023-05-31 08:39:26 +0300

    first display of chats

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,67 +1,71 @@
 import React, { useEffect, useState } from "react";
 import ListItem from './ListItem'
 import { auth } from "../../firebase";
-import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
+import { collection, where, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "../../firebase";
 import { getAuth } from "firebase/auth";
-
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { DocumentData } from "@firebase/firestore-types";
+
 
 export default function List() {
-  interface Chats_Face{
-      chatName: string | undefined,
-      otherUserId: string | undefined,
+  interface CHAT_NAME{
+    name: string
   }
-  const [user, setUser] = useState({
-    userId: auth.currentUser?.uid,
-    userName: auth.currentUser?.displayName
-  })
-  const [chats, setChats] = useState<Chats_Face>()
-  const [otherUser, setOtherUser] = useState({
-    pfp: '',
-    name: ''
-  })
-
-  const getChatsInfo = async () => {
-   const q = query(collection(db, 'chats'), where(String(user.userName) , '==', user.userId))
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-    setChats({
-      chatName: doc.get('chatName'),
-      otherUserId: doc.get('chatName').replace(auth.currentUser?.uid, ''),
-      })
-    })
+  interface USER_CHATS{
+    sentBy: string,
+    text: string,
+    timestamp: string
   }
+  const [chatN, setChatN] = useState<CHAT_NAME[]>([])
+  const[userChat, setUserChats] = useState<USER_CHATS[]>([])
+  
+  if(auth.currentUser?.displayName != null || undefined){
+    const Ref = collection(db, 'chats')
+    const q = query(Ref, where(String(auth.currentUser?.displayName), '==', auth.currentUser?.uid))
+    const [chats] = useCollectionData(q)
 
-  const getPfp = async() => {
-    const q = query(collection(db, 'users'), where('uID' , '==', chats?.otherUserId))
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.data());
-      setOtherUser({
-        pfp: doc.get('PhotoURL'),
-        name: doc.get('uName')
+    
+    const getChatRooms = async () => {
+      for(let i = 0; i <= chatN.length; i++){
+        const querySnapshot = await getDocs(collection(db, `${chatN[i]}`))
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data())
+          setUserChats(k => [...k, {
+            sentBy: doc.get('sentBy'),
+            text: doc.get('text'),
+            timestamp: doc.get('timestamp')
+          }])
+        });
+      }
+    }
+
+    useEffect(() => {
+      chats?.map(e => {
+        setChatN(k => [...k, e.chatName])
       })
-      })
+    }, [chats])
+   
+    useEffect(() => {
+     getChatRooms()
+    }, [chatN])
   }
+    
+    // if sentBy field != currentUserUid => get User pfp
+ console.log('Chat info: ', userChat);
 
-  useEffect(() => {
-    getChatsInfo()
-  }, [user])
-  useEffect(() => {
-    getPfp()
-  }, [chats])
-
-
+ const display = userChat.map(e => (
+  <ListItem 
+    text = {e.text}
+    sentBy = {e.sentBy}
+  />
+ ))
 return (
     <div className='max-w-full max-h-screen px-2 md:m-0 '>
-        <ListItem 
-        img = {otherUser.pfp}
-        name = {otherUser.name}
-        chatName = {chats?.chatName}
-        />
+      {display}
     </div>
   )
 }
--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -5,25 +5,16 @@ import { query, collection, orderBy } from 'firebase/firestore'
 import { db } from '../../firebase'
 
 export default function ListItem(props: any) {  
-  const [lastMasg, setlastMsg] = useState('')
-  const messagesRef = collection(db, `${props.chatName}`)
-  const q = query(messagesRef, orderBy('timestamp', 'asc'))
-  const [messages] = useCollectionData(q)
-
- useEffect(() => {
-  messages?.map(e => {
-    setlastMsg(e.text)
-  })
- }, [messages])
+ 
   return (
     <div className='container flex items-center  mt-2 gap-2'>
         <div className='w-9 h-9 rounded-full bg-red-500 flex-shrink-0'> 
-          {<img src={props.img} alt="" className='w-9 h-9 rounded-full flex-shrink-0' />}
+          {/* {<img src={props.img} alt="" className='w-9 h-9 rounded-full flex-shrink-0' />} */}
         </div>
         
         <div>
-            <h4>{props.name}</h4>
-            <p className='text-xs'>{lastMasg}</p>
+            <h4>{props.sentBy}</h4>
+            <p className='text-xs'>{props.text}</p>
         </div>
         <div className='items-baseline ml-auto'>
             {/* <p className='text-xxs'>16:45</p> */}
